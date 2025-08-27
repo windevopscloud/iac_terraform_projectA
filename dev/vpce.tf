9e3c84902b4d799dee1e03f299f98f7e77cb26da
windevopscloud <windevopscloud@gmailcom>
2025-08-28 00:15:31 +0400

    first

--- dev/vpce.tf
+++ dev/vpce.tf
@@ -1,6 +1,6 @@
 resource "aws_vpc_endpoint" "ssm" {
   vpc_id              = data.terraform_remote_state.bootstrap.outputs.vpc_id[0].id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssm"
+  service_name        = "com.amazonaws.${aws_region}.ssm"
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
 
@@ -14,7 +14,7 @@ resource "aws_vpc_endpoint" "ssm" {
 
 resource "aws_vpc_endpoint" "ssm_messages" {
   vpc_id              = data.terraform_remote_state.bootstrap.outputs.vpc_id[0].id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.ssmmessages"
+  service_name        = "com.amazonaws.${aws_region}.ssmmessages"
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
 
@@ -28,7 +28,7 @@ resource "aws_vpc_endpoint" "ssm_messages" {
 
 resource "aws_vpc_endpoint" "ec2_messages" {
   vpc_id              = data.terraform_remote_state.bootstrap.outputs.vpc_id[0].id
-  service_name        = "com.amazonaws.${data.aws_region.current.name}.ec2messages"
+  service_name        = "com.amazonaws.${aws_region}.ec2messages"
   vpc_endpoint_type   = "Interface"
   private_dns_enabled = true
 

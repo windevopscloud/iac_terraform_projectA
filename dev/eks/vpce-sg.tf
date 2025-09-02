c32038a36de01b00b617bc12fd6df39013bbe15b
windevopscloud <windevopscloud@gmailcom>
2025-09-02 22:37:51 +0400

    first

--- dev/eks/vpce-sg.tf
+++ dev/eks/vpce-sg.tf
@@ -19,5 +19,27 @@ resource "aws_security_group" "vpc_endpoint" {
     security_groups = [aws_security_group.eks_nodes_sg.id]
   }
 
-  tags = merge(var.tags, { Name = "vpc-endpoint-sg" })
+  tags = merge(var.tags, { Name = "vpc-endpoint-sg-eks" })
+}
+
+# For vpce-sg-eks security group  
+resource "aws_security_group_rule" "eks_vpce_allow_github_runner" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = data.aws_security_group.github_runner.id
+  security_group_id        = aws_security_group.vpc_endpoint.id  # vpce-sg-eks
+  description              = "Allow HTTPS from GitHub runner to EKS VPC endpoints"
+}
+
+# For vpce-sg-ssm security group  
+resource "aws_security_group_rule" "ssm_vpce_allow_github_runner" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  source_security_group_id = data.aws_security_group.github_runner.id
+  security_group_id        = data.aws_security_group.vpc_endpoint_ssm.id  # vpce-sg-ssm
+  description              = "Allow HTTPS from GitHub runner to SSM VPC endpoints"
 }

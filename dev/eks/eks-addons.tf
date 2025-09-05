aaf7c52144d35feaf9107aa9a53a750f610afc5f
windevopscloud <windevopscloud@gmailcom>
2025-09-05 12:24:17 +0400

    first

--- dev/eks/eks-addons.tf
+++ dev/eks/eks-addons.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_addon" "coredns" {
   cluster_name      = aws_eks_cluster.this.name
   addon_name        = "coredns"
-  addon_version     = "v1.10.1-eksbuild.1"
+  addon_version     = "v1.11.4-eksbuild.22"
   resolve_conflicts = "OVERWRITE"
 
   depends_on = [aws_eks_node_group.this]
@@ -10,7 +10,7 @@ resource "aws_eks_addon" "coredns" {
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name      = aws_eks_cluster.this.name
   addon_name        = "kube-proxy"
-  addon_version     = "v1.27.4-eksbuild.1"
+  addon_version     = "v1.30.14-eksbuild.8"
   resolve_conflicts = "OVERWRITE"
 
   depends_on = [aws_eks_node_group.this]
@@ -19,7 +19,7 @@ resource "aws_eks_addon" "kube_proxy" {
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name      = aws_eks_cluster.this.name
   addon_name        = "vpc-cni"
-  addon_version     = "v1.14.1-eksbuild.1"
+  addon_version     = "v1.20.1-eksbuild.3"
   resolve_conflicts = "OVERWRITE"
 
   depends_on = [aws_eks_node_group.this]

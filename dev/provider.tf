47c2dc7e0804e3c66f4de958961091710096fdbb
windevopscloud <windevopscloud@gmailcom>
2025-09-08 21:54:08 +0400

    first

--- dev/provider.tf
+++ dev/provider.tf
@@ -38,8 +38,8 @@ provider "kubernetes" {
 # -----------------------------
 provider "helm" {
   kubernetes {
-    host                   = aws_eks_cluster.this_endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.this_certificate_authority_data)
+    host                   = aws_eks_cluster.this.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }

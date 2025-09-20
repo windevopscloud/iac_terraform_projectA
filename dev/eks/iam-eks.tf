7c2579edab7b3fcfd977385f9b8dbadd1d5bac4c
windevopscloud <windevopscloud@gmailcom>
2025-09-20 13:47:02 +0400

    first

--- dev/eks/iam-eks.tf
+++ dev/eks/iam-eks.tf
@@ -146,7 +146,8 @@ resource "aws_iam_role_policy_attachment" "ebs_csi_driver" {
 # OIDC Provider for IRSA
 resource "aws_iam_openid_connect_provider" "eks" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  #thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  thumbprint_list = ["9e99a48a9960b14926bb7f3b02e22da2b0ab7280"] # Standard EKS thumbprint
   url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
 
   depends_on = [aws_eks_cluster.this]
